ab72f81e28413e4f1cb6a71ee7c4c172fe8ea08a
tss <tss@debian-tss24>
2016-11-14 11:14:15 +0100

    fourth commit aggiunta classe con lezione su commenti e javadoc

---
+++ src/lezione2/Main.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lezione2;
+
+/**
+ * Esempio di commenti in Java
+ * @author tss
+ */
+public class Main {
+    /**
+     * Da questo metodo parte l'esecuzione del programma
+     * @param args 
+     */
+    public static void main(String[] args) {
+        System.out.println("lezione2 started...");
+        //commento su una riga
+        
+        /*
+        commento
+        su
+        più
+        righe
+        */
+        
+        /**
+         * commento javadoc: serve come descrizione del programma e viene tenuto in considerazione per creare una sorta di manuale/documentazione.
+         * Solitamente viene inserito prima della classe e prima di ogni metodo per spiegare la loro funzionalità e la descrizione
+         */
+    }
+    
+}
